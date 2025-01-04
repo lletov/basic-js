759f7f81428f942fbc88a190eb01d1ce4f697c65
Dmitry <dmitry2595@gmail.com>
2025-01-04 15:24:44 +0300

    feat: get seacon partly

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  var timestamp = Date.parse(date);
+
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  const month = date.getMonth();
+  switch (month) {
+    case 0:
+    case 11:
+    case 1:
+      return 'winter'
+    case 2:
+    case 3:
+    case 4:
+      return 'spring'
+    case 5:
+    case 6:
+    case 7:
+      return 'summer'
+    case 8:
+    case 9:
+    case 10:
+      return 'autumn'
+  }
+
+
 }
 
 module.exports = {
